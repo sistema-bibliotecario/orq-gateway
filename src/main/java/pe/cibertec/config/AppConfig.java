48067efac6dec31905e7cdb7440f0113e45257c9
Ramiro Blas <ramiro060694@gmail.com>
2025-02-15 18:09:40 -0500

    feat: se agrego configuracion para config server y token validate

---
+++ src/main/java/pe/cibertec/config/AppConfig.java
@@ -0,0 +1,11 @@
+package pe.cibertec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
